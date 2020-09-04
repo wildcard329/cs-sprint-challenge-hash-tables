53457c020ccdec87e30a440e3c4aa26778067257
wildcard329 <enkiduwildman@gmail.com>
2020-09-04 15:01:44 -0700

    second challenge passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,31 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    itinerary = {}
+    route = []
+    for ticket in tickets:
+        # print(ticket.source, ticket.destination)
+        itinerary[ticket.source] = ticket.destination
+    print(itinerary)
+
+    start = itinerary.get('NONE')
+    route.append(start)
+    next_location = itinerary.get(start)
+    while next_location != 'NONE':
+        route.append(next_location)
+        next_location = itinerary.get(next_location)
+    route.append(next_location)
 
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets, 3)
